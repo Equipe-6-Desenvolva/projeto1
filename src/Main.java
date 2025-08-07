c6d7e057f6c6c78a5a96969a870fcb83b54133fe
Willian Gonçalves <williangoncalves74@gmail.com>
2025-08-07 08:26:16 -0400

    Implementado Avaliacao na main

--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,6 @@
+import Models.Avaliacao;
 import Models.Cliente;
+import View.AvaliacaoConsole;
 import View.ClienteConsole;
 
 public class Main {
@@ -6,5 +8,10 @@ public class Main {
         Cliente cliente = new ClienteConsole().run();
         System.out.println("\nCadastro concluído:");
         System.out.println(cliente);
+
+        AvaliacaoConsole console = new AvaliacaoConsole();
+        Avaliacao avaliacao = console.run();
+        System.out.println("\nAvaliação cadastrada:");
+        System.out.println(avaliacao);
     }
 }
